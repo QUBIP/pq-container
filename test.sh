17ed26d97fc37eb32430fddfc0803d11b1a3577c
gstarovo <gstarovo@redhat.com>
2025-07-16 16:43:37 +0200

    Adjust TMT plan for accurate beakerlib test reporting in testing-farm

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@
 
 POLICY="$(update-crypto-policies --show)"
 PACKAGES="crypto-policies-scripts liboqs openssl curl expect"
-KEY="root.key"
-CRT="root.crt"
+KEY="/etc/pki/nginx/private/server.key" # created and stored during setup phase
+CRT="/etc/pki/nginx/server.crt" # created and stored during setup phase
 SERVER_TXT="server_pid.txt"
 
 # Function to start openssl s_server and return its PID
@@ -32,7 +32,7 @@ function stop_s_server {
     # Kill the openssl s_server process
     if [ -n "$pid" ]; then
         rlLogInfo "Stopping openssl s_server with PID $pid..."
-        kill "$pid" 2>/dev/null || true-
+        kill "$pid" 2>/dev/null || true
         rlWait $pid
         rlLogInfo "Server stopped."
     else
